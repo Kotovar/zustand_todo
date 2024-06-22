9ee6e9c56c1af21c4f27190607a4637d07ca45fa
Kotovar <neasit3@gmail.com>
2024-06-22 16:23:13 +0700

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tslint from "typescript-eslint";
+
+export default tslint.config(
+  eslint.configs.recommended,
+  ...tslint.configs.strict,
+  ...tslint.configs.stylistic,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+
+    rules: {
+      "no-unused-expressions": "error",
+      "no-console": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  { files: ["**/*.{c,m,}{t,j}s", "**/*.tsx"] },
+  eslintConfigPrettier,
+);
